822e874ac8c6afca8d4250da61888413db88525e
cchittul <102188969+cchittul@users.noreply.github.com>
2025-05-08 16:02:30 -0400

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,9 @@
+/*resource "aws_instance" "my_vm" {
+  ami           = "ami-084568db4383264d4" //Ubuntu AMI
+  instance_type = "t2.micro"
+  subnet_id     = "subnet-03e1b31bac6f898e7"
+
+  tags = {
+    Name = "My EC2 instance",
+  }
+}*/
---
+++ variables.tf
@@ -0,0 +1,5 @@
+locals {
+ ami      = "ami-084568db4383264d4"
+ type     = "t2.micro"
+ name_tag = "My EC2 Instance"
+}
